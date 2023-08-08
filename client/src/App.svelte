c18d4c1c718c606c2143da6a40f41c4f4fc427a2
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2023-08-08 23:25:37 +0800

    feat: add basic main menu

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
+    import MainMenu from "./lib/MainMenu.svelte";
     import PokerGame from "./lib/PokerGame.svelte";
+    type Page = "Home" | "Game";
+    let page: Page = "Game";
+
+    function goToPage(newPage: Page) {
+        page = newPage;
+    }
 </script>
 
-<PokerGame />
+{#if page === "Game"}
+    <PokerGame />
+{/if}
+{#if page === "Home"}
+    <MainMenu {goToPage} />
+{/if}
---
+++ client/src/lib/MainMenu.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Button from "./Button.svelte";
+    export let goToPage: (page: "Game") => void;
+</script>
+
+<div
+    class="flex flex-col justify-center items-center gap-4 h-full bg-neutral-300 max-height"
+>
+    <h1 class="text-5xl font-thin">Pocket Poker</h1>
+    <Button action={() => goToPage("Game")}>Play</Button>
+</div>
