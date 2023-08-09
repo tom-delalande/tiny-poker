3ab311fae841dcbb406770a4c4b32172e9ce500e
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2023-08-09 13:46:12 +0800

    fix: add default styles

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,16 +2,18 @@
     import MainMenu from "./lib/MainMenu.svelte";
     import PokerGame from "./lib/PokerGame.svelte";
     type Page = "Home" | "Game";
-    let page: Page = "Game";
+    let page: Page = "Home";
 
     function goToPage(newPage: Page) {
         page = newPage;
     }
 </script>
 
-{#if page === "Game"}
-    <PokerGame />
-{/if}
-{#if page === "Home"}
-    <MainMenu {goToPage} />
-{/if}
+<div class="max-height">
+    {#if page === "Game"}
+        <PokerGame />
+    {/if}
+    {#if page === "Home"}
+        <MainMenu {goToPage} />
+    {/if}
+</div>
