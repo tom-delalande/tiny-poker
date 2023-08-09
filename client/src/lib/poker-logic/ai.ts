40a3257b3996518c297e3033dcc73d1b0f4a0ed2
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2023-08-09 22:58:18 +0800

    feat(ai): simplify ai

--- client/src/lib/poker-logic/ai.ts
+++ client/src/lib/poker-logic/ai.ts
@@ -1,4 +1,10 @@
 import type { PokerState } from "./model";
+import {
+  playerCall,
+  playerCheck,
+  playerFold,
+  playerRaise,
+} from "./player-actions";
 
 export function performEnemyActions(
   seat: number,
@@ -6,85 +12,34 @@ export function performEnemyActions(
 ): PokerState {
   if (pokerState.seats[pokerState.currentAction.seatInTurn].isCurrentPlayer)
     return pokerState;
+
   const seed = Math.random();
+  let mustRespondToRaise = pokerState.currentAction.minRaise > 0;
 
-  const player = pokerState.seats[seat];
-  let mustCall = pokerState.currentAction.minRaise > 0;
-
-  // Fold
-  if (mustCall && seed < 0.1) {
-    console.debug({
-      message: "Opponent Folded",
-      seed: seed,
-      minRaise: pokerState.currentAction.minRaise,
-    });
-    pokerState.seats[seat].lastAction = "Fold";
-    pokerState.seats[seat].out = true;
-    return pokerState;
+  // If no raise is currently active, 1/2 chance to Raise / Check
+  if (!mustRespondToRaise) {
+    if (seed < 0.5 || pokerState.seats[seat].stack === 0) {
+      return playerCheck(seat, pokerState);
+    } else {
+      const amount = Math.min(
+        pokerState.seats[seat].stack,
+        pokerState.currentAction.minRaise + Math.floor((Math.random() * 10) % 5)
+      );
+      return playerRaise(seat, pokerState, amount);
+    }
   }
 
-  // Call
-  if (mustCall && seed < 0.6) {
-    pokerState.seats[seat].lastAction = "Call";
-    const callAmount = Math.min(
-      pokerState.seats[seat].stack,
-      pokerState.currentAction.minRaise - pokerState.seats[seat].currentRaise
-    );
-    pokerState.seats[seat].stack -= callAmount;
-    pokerState.pot += callAmount;
-    console.debug({
-      message: "Opponent Called",
-      seed: seed,
-      minRaise: pokerState.currentAction.minRaise,
-      currentRaise: player.currentRaise,
-      opponentStack: pokerState.seats[seat].stack,
-      callAmount: callAmount,
-    });
-    return pokerState;
+  // If a raise is active, 50% - 15% - 35% chance to Call - Raise - Fold
+  if (seed < 0.5) {
+    return playerCall(seat, pokerState);
   }
 
-  // Check
-  if (!mustCall && seed < 0.5) {
-    pokerState.seats[seat].lastAction = "Check";
-    console.debug({
-      message: "Opponent Checked",
-      seed: seed,
-      minRaise: pokerState.currentAction.minRaise,
-    });
-    return pokerState;
+  if (seed < 0.65 || pokerState.seats[seat].stack === 0) {
+    return playerFold(seat, pokerState);
   }
-
-  if (
-    pokerState.seats[seat].stack <= pokerState.currentAction.minRaise ||
-    pokerState.currentAction.lastSeatToRaise === seat
-  ) {
-    pokerState.seats[seat].lastAction = "Fold";
-    pokerState.seats[seat].out = true;
-    console.debug({
-      message: "Opponent Folded",
-      seed: seed,
-      opponentStack: pokerState.seats[seat].stack,
-      minRaise: pokerState.currentAction.minRaise,
-    });
-    return pokerState;
-  }
-  // Raise
-  pokerState.seats[seat].lastAction = "Raise";
-  const raiseAmount = Math.min(
+  const amount = Math.min(
     pokerState.seats[seat].stack,
-    pokerState.currentAction.minRaise + Math.floor(seed * 10)
+    pokerState.currentAction.minRaise + Math.floor(seed * 5)
   );
-  pokerState.seats[seat].stack -= raiseAmount;
-  pokerState.pot += raiseAmount;
-  pokerState.currentAction.minRaise = raiseAmount + player.currentRaise;
-  pokerState.seats[seat].currentRaise = pokerState.currentAction.minRaise;
-  pokerState.currentAction.lastSeatToRaise = seat;
-  console.debug({
-    message: "Opponent Raised",
-    seed: seed,
-    opponentStack: pokerState.seats[seat].stack,
-    minRaise: pokerState.currentAction.minRaise,
-    raiseAmount: raiseAmount,
-  });
-  return pokerState;
+  return playerRaise(seat, pokerState, amount);
 }
