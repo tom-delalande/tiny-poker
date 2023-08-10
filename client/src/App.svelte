7b6fae7a1f97098ee9a5cd823aaedfa8733eb8bc
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2023-08-10 09:59:22 +0800

    fix: fix viewport on sim/safari

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -13,7 +13,17 @@
 
     onMount(() => {
         loadAudio();
+
+        // fix viewport height
+        // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+        setAppHeight();
+        window.addEventListener("resize", setAppHeight);
     });
+
+    function setAppHeight() {
+        const doc = document.documentElement;
+        doc.style.setProperty("--vh", window.innerHeight * 0.01 + "px");
+    }
 </script>
 
 <div class="app-container">
