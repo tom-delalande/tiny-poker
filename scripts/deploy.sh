2d0c25e5ddc3d2de8a37d23f741ad1e7b64b1375
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2023-08-18 21:00:58 +0800

    feat(ci): only update when there are changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
-git pull
-
-docker compose build server nginx client
-docker compose run client
-~/.docker/cli-plugins/docker-rollout server
+if [[ `git status --porcelain` ]]; then
+    git pull
+    docker compose build server nginx client
+    docker compose run client
+    ~/.docker/cli-plugins/docker-rollout server
+fi
