953030fbc0d614e89e16741b9453c80998390770
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2023-08-18 20:50:52 +0800

    feat(ci): run with docker

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,37 +1,5 @@
-deploy_nginx() {
-    if [ -n "$(lsof -t -i :9029)" ]; then
-        docker exec nginx nginx -s reload
-    else
-        docker compose up --build
-    fi
-}
+git pull
 
-deploy_client() {
-    cd client
-    npm install
-    npm run build
-    cd ..
-}
-
-deploy_server() {
-    cd server
-    go build -o app src/main.go
-    if [ -n "$(lsof -t -i :9001)" ]; then
-        old_port=9001
-        new_port=9002
-    else
-        old_port=9002
-        new_port=9001
-    fi
-
-    PORT=$new_port ./app &>/dev/null &
-    old_pid=$(lsof -t -i :$old_port)
-
-    if [ -n "$old_pid" ]; then
-        kill $old_pid
-    fi
-    cd ..
-}
-
-deploy_client
-deploy_server
+docker compose build server nginx client
+docker compose run client
+~/.docker/cli-plugins/docker-rollout server
