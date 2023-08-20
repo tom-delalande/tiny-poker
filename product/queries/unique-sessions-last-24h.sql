e61afb5d5bdc9c7599fd6fde97e6424911030e78
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2023-08-20 23:37:06 +0800

    fix(product): fix sessions query

--- product/queries/unique-sessions-last-24h.sql
+++ product/queries/unique-sessions-last-24h.sql
@@ -1,2 +1,2 @@
 SELECT "Unique sessions in last 24 hours";
-SELECT COUNT(DISTINCT(session_id)) FROM event_logs WHERE instant > datetime('now', '-24 hour');
+SELECT COUNT(DISTINCT(session_id)) FROM event_logs WHERE timestamp > datetime('now', '-24 hour');
