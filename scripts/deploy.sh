eace04727023626d42cdb98c2b9e4424c296d537
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2023-08-21 20:52:01 +0800

    fix(ci): swap deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,6 @@ echo "Fetching remote repository..."
 git fetch
 
 if git diff-index --quiet HEAD --; then
-    echo "No changes detected in git"
-else
     BUILD_VERSION=$(git rev-parse HEAD)
     echo "Changes detected, deploying new version: $BUILD_VERSION"
     git pull
@@ -19,4 +17,6 @@ else
     echo "Releasing new server version"
     BUILD_VERSION=$BUILD_VERSION docker rollout server
     BUILD_VERSION=$BUILD_VERSION docker-compose up -d --no-deps --scale server=1 --no-recreate server
+else
+    echo "No changes detected in git"
 fi
