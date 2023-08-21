a79d38a50d1ae8a882c772292e90b629f2a45e9e
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2023-08-21 15:20:37 +0800

    fix(ci): fix deploy script for alpine

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
+git fetch
+
 if [[ `git status --porcelain` ]]; then
     git pull
-    docker compose build server nginx client
-    docker compose run client
+    docker-compose build server nginx client
+    docker-compose run client
     ~/.docker/cli-plugins/docker-rollout server
 fi
