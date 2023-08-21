91b0d900340110d478574214fa70ba66a96561d5
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2023-08-21 19:57:40 +0800

    chore(ci): add logs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+echo "Fetching remote repository..."
 git fetch
 
 if [[ `git status --porcelain` ]]; then
+    BUILD_VERSION = $(git rev-parse HEAD)
+    echo "Changes detected, deploying new version: $BUILD_VERSION"
     git pull
+
+    echo "Running docker build"
     docker-compose build server nginx client
-    docker-compose run -e BUILD_VERSION=$(git rev-parse HEAD) client
-    ~/.docker/cli-plugins/docker-rollout -e BUILD_VERSION=$(git rev-parse HEAD) server
+
+    echo "Running client container"
+    docker-compose run client
+
+    echo "Releasing new server version"
+    docker rollout server
+else
+    echo "No changes detected in git"
 fi
