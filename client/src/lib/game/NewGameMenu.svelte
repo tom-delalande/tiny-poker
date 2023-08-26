9f126d127e87403adba0f1e1933e232bfe8e5e03
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2023-08-26 09:06:41 +0800

    feat(client): improve rebuy menu positioning

--- client/src/lib/game/NewGameMenu.svelte
+++ client/src/lib/game/NewGameMenu.svelte
@@ -7,14 +7,32 @@
     export let gameFinished: boolean;
     export let nextHand: () => void;
     export let bot: BotInformation;
+
+    let rebuyMenuShowing = false;
 </script>
 
 {#if gameFinished}
     <div class="flex flex-col items-center gap-4">
-        <BuyInMenu {bot} />
-        <Button action={() => router.set({ route: "BotSelectionScreen" })}
-            >Exit</Button
-        >
+        {#if rebuyMenuShowing}
+            <div
+                class="absolute bottom-36 bg-neutral-300 p-4 border-2
+            border-neutral-500 rounded-md bg-opacity-90"
+            >
+                <BuyInMenu {bot} />
+            </div>
+        {/if}
+        <div class="flex gap-2">
+            <Button action={() => router.set({ route: "BotSelectionScreen" })}
+                >Exit</Button
+            >
+            {#if rebuyMenuShowing}
+                <Button action={() => (rebuyMenuShowing = false)}>Hide</Button>
+            {:else}
+                <Button action={() => (rebuyMenuShowing = true)}>
+                    Play Again</Button
+                >
+            {/if}
+        </div>
     </div>
 {:else}
     <Button action={nextHand}>Continue</Button>
