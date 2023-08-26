67be163217a04147b6bc71e4349f874e9bd18ebc
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2023-08-26 19:38:30 +0800

    fix(client): fix avatar position

--- client/src/lib/GenericAvatar.svelte
+++ client/src/lib/GenericAvatar.svelte
@@ -8,7 +8,7 @@
 <button
     on:click={openCharacterCard}
     class="flex flex-col items-center justify-center gap-2
-    p-2 rounded-md active:scale-90 transition"
+    p-2 rounded-md active:scale-90 transition w-full"
 >
     <div class="flex items-center justify-center gap-2">
         <p class="text-sm">{name}</p>
