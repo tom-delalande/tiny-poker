364dcdbfe1872b8608069e99619261b046866b17
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2023-08-29 22:35:34 +0800

    fix(client): remove hover on buttons

--- client/src/lib/Button.svelte
+++ client/src/lib/Button.svelte
@@ -13,7 +13,6 @@
 <button
     class={`bg-gray-50 rounded-md active:scale-90 transition
     disabled:bg-neutral-300 disabled:border-2 disabled:border-gray-50
-    hover:bg-red-200 hover:drop-shadow-lg
     disabled:active:scale-100 ${$$props.class}`}
     {disabled}
     on:click={performAction}><slot /></button
